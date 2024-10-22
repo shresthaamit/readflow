7cfcfb9acbe9f7d003655c688059f10cc16c9375
AmitStha <shresthaamit273@gmail.com>
2024-10-22 09:52:15 +0545

    Book Section added

--- src/pages/books.jsx
+++ src/pages/books.jsx
@@ -1,12 +1,39 @@
 import React from "react";
 import Footer from "../components/footer";
 import Navbar from "../components/navbar";
+import "./books.css";
+import books from "./books.json";
 
 function Books() {
   return (
     <>
-      <div>
-        <h2>This is books page</h2>
+      <h1 className="title">Find The Perfect Book</h1>
+      <div className="booksection">
+        <div className="booksearch">
+          <h1>Sesg</h1>
+        </div>
+        <div className="books">
+          {books.map((book) => (
+            <book>
+              <div className="bookimg">
+                <img src={book.img} alt={book.bookTitle} />
+              </div>
+              <div className="carddetail">
+                <div className="detail1">
+                  <img src={book.star} alt="rating star" />
+                  <span>{book.rating}</span>
+                  <span className="span">(6) • </span>
+                  <span className="span">{book.category}</span>
+                </div>
+
+                <p className="author"> {book.author}</p>
+              </div>
+              <p>
+                <span className="book-title">{book.bookTitle}</span>
+              </p>
+            </book>
+          ))}
+        </div>
       </div>
     </>
   );
