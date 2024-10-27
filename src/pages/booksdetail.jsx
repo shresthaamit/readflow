8683d8c929f24683d0b02a34106cc1db5d046d7d
AmitStha <shresthaamit273@gmail.com>
2024-10-27 19:31:39 +0545

    Book Detail page added

--- src/pages/booksdetail.jsx
+++ src/pages/booksdetail.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import books from "./books.json";
+import { LuDownload } from "react-icons/lu";
+import { BsFillBookmarkHeartFill } from "react-icons/bs";
 import "./books.css";
 export default function BookDetails() {
   const { id } = useParams();
@@ -10,7 +12,7 @@ export default function BookDetails() {
   }
   return (
     <div className="detailpage">
-      <h2>Book Details</h2>
+      {/* <h2>Book Details</h2> */}
       <div className="bookdetail">
         <div className="bookimg">
           <img src={book.img} alt={book.title} />
@@ -20,8 +22,21 @@ export default function BookDetails() {
           <h3>{book.author}</h3>
           <h5>{book.rating}</h5>
           <p>{book.details}</p>
+          <div className="bookbuttons">
+            <button className="buttons ">
+              Download
+              <span>
+                <LuDownload />
+              </span>
+            </button>
+            <button className="buttons style">
+              Add to favourite
+              <span>{<BsFillBookmarkHeartFill />}</span>
+            </button>
+          </div>
         </div>
       </div>
+
       {/* <p>Book ID: {id}</p> */}
       {/* <p>Author: {book.author}</p> */}
     </div>
