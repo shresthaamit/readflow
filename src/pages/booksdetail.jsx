6ae2d3878120933db212de45c5ccf673709651f0
AmitStha <shresthaamit273@gmail.com>
2024-10-29 20:02:08 +0545

    Book Rating section added

--- src/pages/booksdetail.jsx
+++ src/pages/booksdetail.jsx
@@ -4,6 +4,8 @@ import books from "./books.json";
 import { LuDownload } from "react-icons/lu";
 import { BsFillBookmarkHeartFill } from "react-icons/bs";
 import "./books.css";
+// import QRCode from "qrcode.react";
+import placeholderQR from "../images/qr.png";
 export default function BookDetails() {
   const { id } = useParams();
   const book = books.find((b) => b.id === parseInt(id));
@@ -33,6 +35,39 @@ export default function BookDetails() {
               Add to favourite
               <span>{<BsFillBookmarkHeartFill />}</span>
             </button>
+          </div>
+          <div className="ratesection">
+            <div className="ratereview">
+              <textarea placeholder="Enter a review"></textarea>
+              <div className="rating-submit">
+                <select name="rating" className="rating-dropdown">
+                  <option value="">Rate</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                </select>
+                <button type="button" className="send-button">
+                  Send
+                </button>
+              </div>
+            </div>
+            {/* <div className="qrsection"> */}
+            <div className="qr-section">
+              {/* <h4>QR Code</h4> */}
+              <img
+                src={placeholderQR}
+                alt="QR Code Placeholder"
+                style={{
+                  maxWidth: "128px",
+                  width: "100%",
+                  padding: "16px",
+                  background: "white",
+                }}
+              />
+            </div>
+            {/* </div> */}
           </div>
         </div>
       </div>
